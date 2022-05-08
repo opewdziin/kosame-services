0f207405cad0ebae683286f3efe34f2531e8ce2d
Pewd <99264134+opewdziin@users.noreply.github.com>
2022-05-08 00:24:56 -0300

    new language system, version 0.2

---
+++ src/main/java/com/kosame/pewd/services/language/en_US.java
@@ -0,0 +1,8 @@
+package com.kosame.pewd.services.language;
+
+public class en_US {
+
+    // private message text
+    public static String privatemessage$receivement = "§8[Message from {player}§8]§7: §6{message}";
+    public static String privatemessage$envio = "§8[Message to {player}§8]§7: §6{message}";
+}
---
+++ src/main/java/com/kosame/pewd/services/language/pt_BR.java
@@ -0,0 +1,8 @@
+package com.kosame.pewd.services.language;
+
+public class pt_BR {
+
+    // texto da mensagem privada
+    public static String mensagemprivada$recebimento = "§8[Mensagem de {jogador}§8]§7: §6{mensagem}";
+    public static String mensagemprivada$envio = "§8[Mensagem para {jogador}§8]§7: §6{mensagem}";
+}
