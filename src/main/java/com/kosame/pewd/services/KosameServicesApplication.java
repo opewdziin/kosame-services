00391cf465a7a24e634f6e2bf2a888cda0566831
Pewd <99264134+opewdziin@users.noreply.github.com>
2022-05-07 23:51:00 -0300

    Initial commit

---
+++ src/main/java/com/kosame/pewd/services/KosameServicesApplication.java
@@ -0,0 +1,13 @@
+package com.kosame.pewd.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosameServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosameServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosame/pewd/services/KosameServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosame.pewd.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosameServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
